47558dc83d5e7652551da5f000feda40d1a5a4fe
abregman <abregman@redhat.com>
2018-08-15 16:09:59 +0300

    Add common module for handling clients
    Also, updated requirements with a new package being used
    and added a LICENSE file.
    
    Change-Id: I66682d169a30ba1d0d5ce7ecfa24eb3ba5c511e4

---
+++ tobiko/common/clients.py
@@ -0,0 +1,40 @@
+# Copyright 2018 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os
+
+from heatclient import client as heat_client
+from keystoneauth1 import loading
+from keystoneauth1 import session
+
+
+class ClientManager(object):
+    """Manages OpenStack official Python clients."""
+
+    def __init__(self):
+        self.heat_client = self.get_heat_client()
+
+    def get_heat_client(self):
+        """Returns heat client."""
+
+        sess = self.get_session()
+        return heat_client.Client('1', session=sess)
+
+    def get_session(self):
+        """Returns keystone session."""
+        loader = loading.get_plugin_loader('password')
+        auth = loader.load_from_options(auth_url=os.environ['OS_AUTH_URL'],
+                                        username=os.environ['OS_USERNAME'],
+                                        password=os.environ['PASSWORD'],
+                                        project_id=os.environ['OS_TENANT_ID'])
+        return session.Session(auth=auth)
