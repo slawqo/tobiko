0377537dc0b447882a6f98a33fe845c72bd8ab32
Federico Ressi <fressi@redhat.com>
2018-08-23 08:01:39 +0200

    Create tox.ini file.
    Configure with minimal requirements for running -
      pep8 - flake8 verifications
      py27/py35 - unittests
    
    Change-Id: If09bab1f0a8501afddd720cf9ea9335867072e1d

--- tobiko/tests/scenario/base.py
+++ tobiko/tests/scenario/base.py
@@ -12,8 +12,10 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 import os
 import subprocess
+
 from tempest.common.utils import net_utils
 from tempest.lib.common.utils import test_utils
 
