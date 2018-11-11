9bdc604bf524f7ffdcfdc31e55173e68de4ef774
abregman <abregman@redhat.com>
2018-11-11 14:48:59 +0200

    Add a cleanup utility
    Since tobiko is not cleaning up resources, added an alternative
    that should ease development and testing processes.
    
    Also updated setup.cfg with entries for cleanup and creation utils.
    
    Change-Id: Ie88f79108d83723d107b1c5ed30ac69f19318820

---
+++ tobiko/cmd/delete.py
@@ -0,0 +1,63 @@
+# Copyright 2018 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import argparse
+import logging
+import sys
+
+from tobiko.cmd import base
+
+LOG = logging.getLogger(__name__)
+
+
+class DeleteUtil(base.TobikoCMD):
+
+    def __init__(self):
+        super(DeleteUtil, self).__init__()
+        self.parser = self.get_parser()
+        self.args = (self.parser).parse_args()
+
+    def get_parser(self):
+        parser = argparse.ArgumentParser(add_help=True)
+        parser.add_argument(
+            '--stack', '-s',
+            help="The name of the stack to remove.")
+        parser.add_argument(
+            '--all', '-a', action='store_true', dest='all',
+            help="Remove all the stacks created by Tobiko.")
+        return parser
+
+    def delete_stack(self, stack_name=None, all_stacks=False):
+        """Deletes a stack based on given arguments."""
+        if all_stacks:
+            tobiko_stacks = self.stackManager.get_templates_names(
+                strip_suffix=True)
+            stacks = self.stackManager.client.stacks.list()
+            for stack in stacks:
+                if stack.stack_name in tobiko_stacks:
+                    self.stackManager.delete_stack(stack.id)
+                    LOG.info("Deleted stack: %s" % stack.stack_name)
+        else:
+            self.stackManager.delete_stack(stack_name)
+            LOG.info("Deleted stack: %s" % stack_name)
+
+
+def main():
+    """Delete CLI main entry."""
+    delete_cmd = DeleteUtil()
+    delete_cmd.delete_stack(delete_cmd.args.stack,
+                            delete_cmd.args.all)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
