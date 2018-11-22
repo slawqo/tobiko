0f702b52c9b5378afba8518d99655d108a0bede6
Federico Ressi <fressi@redhat.com>
2018-11-22 13:02:12 +0100

    Get config options from environment first, then tobiko.conf, then tempest.conf
    Create a global configuration manager that is able to recollect
    configuration options for tree sources:
      - OS environment variables
      - a new tobiko.conf file
      - the legacy tempest.conf file
    
    Mount environment variables on CONF.environ.<variable>
    Mount tobiko options on CONF.tobiko.<group>.<option>
    Mount tempest options on CONF.tempest.<group>.<option>
    
    Provide a special function to look into multiple options getting
    the first definition. For exaple to get identiy API username it is
    now possible to write:
    
     username = config.get_any_option('environ.OS_USERNAME',
                                      'tempest.auth.username',
                                      'tempest.auth.admin_username')
    
    That would take the value from the first available location from any of
    below locations (in this order):
      - OS_USERNAME environment variable
      - username from [auth] section of tempest.conf
      - admin_username from [auth] section of tempest.conf
      - None if not found in any of above locations
    
    Change-Id: I26488ed4279a21aa151f7828475c4e4461784e48

--- tobiko/config.py
+++ tobiko/config.py
@@ -11,24 +11,136 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+import importlib
+import os
+
 from oslo_config import cfg
+from oslo_log import log
 
-from tempest import config
+CONFIG_MODULES = ['oslo_log.log',
+                  'tobiko.tests.config']
+
+CONFIG_DIRS = [os.getcwd(),
+               os.path.expanduser("~/.tobiko"),
+               '/etc/tobiko']
 
 
-CONF = config.CONF
+class GlobalConfig(object):
+
+    # this is a singletone
+    _instance = None
+    _sources = {}
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
+    def __setattr__(self, name, conf):
+        if conf is None:
+            raise TypeError("Config source cannot be None")
+        actual = self._sources.setdefault(name, conf)
+        if actual is not conf:
+            msg = "Config source already registered: {!r}".format(name)
+            raise RuntimeError(msg)
+
+    def __getattr__(self, name):
+        sources = self._sources.get(name)
+        if sources is None:
+            msg = "Config source not registered: {!r}".format(name)
+            raise NoSuchConfigSource(msg)
+        return sources
 
 
-TobikoGroup = [
-    cfg.StrOpt('floating_network_name',
-               default='public',
-               help="Floating network name "),
-    cfg.StrOpt('admin_username',
-               help="Username to use for admin API requests."),
-    cfg.StrOpt('user_domain_name',
-               default="Default",
-               help="User domain name")
-]
+CONF = GlobalConfig()
 
-tobiko_group = cfg.OptGroup(name="tobiko_plugin",
-                            title="Tobiko Plugin Options")
+
+def init_config():
+    init_tobiko_config()
+    init_tempest_config()
+    init_environ_config()
+
+
+def init_tobiko_config(default_config_dirs=CONFIG_DIRS, product_name='tobiko',
+                       version='unknown'):
+    conf = cfg.ConfigOpts()
+    register_options(conf=conf)
+    conf(args=[], default_config_dirs=default_config_dirs)
+    setup(conf=conf, product_name=product_name, version=version)
+    CONF.tobiko = conf
+
+
+def register_options(conf):
+    for module_name in CONFIG_MODULES:
+        module = importlib.import_module(module_name)
+        register_options_func = getattr(module, 'register_options', None)
+        if register_options_func:
+            register_options_func(conf=conf)
+
+    def dummy_log_register_options(conf):
+        pass
+    log.register_options = dummy_log_register_options
+
+
+def setup(conf, product_name, version):
+    for module in CONFIG_MODULES:
+        setup_func = getattr(module, 'setup', None)
+        if setup_func:
+            setup_func(conf=conf, product_name=product_name, version=version)
+
+    def dummy_log_setup(conf, product_name, version=None):
+        pass
+    log.setup = dummy_log_setup
+
+
+def init_tempest_config():
+    try:
+        from tempest import config
+    except ImportError:
+        pass
+    else:
+        CONF.tempest = tempest_conf = config.CONF
+        if 'http_proxy' not in os.environ or 'https_proxy' not in os.environ:
+            if tempest_conf.service_clients.proxy_url:
+                os.environ['http_proxy'] = (
+                    tempest_conf.service_clients.proxy_url)
+
+
+def init_environ_config():
+    CONF.environ = EnvironConfig()
+
+
+class EnvironConfig(object):
+
+    def __getattr__(self, name):
+        try:
+            os.environ[name]
+        except KeyError:
+            msg = "Environment variable not found: {!r}".format(name)
+            raise cfg.NoSuchOptError(msg)
+
+
+class NoSuchConfigSource(AttributeError):
+    pass
+
+
+def get_any_option(*sources, **kwargs):
+    default = kwargs.get('default', None)
+    for source in sources:
+        value = CONF
+        for name in source.split('.'):
+            try:
+                value = getattr(value, name)
+            except (NoSuchConfigSource,
+                    cfg.NoSuchOptError,
+                    cfg.NoSuchGroupError):
+                break
+
+        else:
+            if value != default:
+                return value
+    return default
+
+
+init_config()
