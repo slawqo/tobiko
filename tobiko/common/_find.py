2357ec1ee08c05c23d3986bc142d3e542d9cedc1
Federico Ressi <fressi@redhat.com>
2019-07-25 11:02:06 +0200

    Add function to select objects by attribute values.
    Change-Id: Ie1bc1ed16c6128cb87380b34d42104edfffa44a2

---
+++ tobiko/common/_find.py
@@ -0,0 +1,27 @@
+# Copyright 2019 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import absolute_import
+
+
+def find_by_attributes(objects, **attributes):
+    if attributes:
+        selection = []
+        for obj in objects:
+            for key, value in attributes.items():
+                if value != getattr(obj, key):
+                    break
+            else:
+                selection.append(obj)
+        objects = selection
+    return objects
