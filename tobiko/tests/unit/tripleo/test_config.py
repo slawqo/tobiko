b7ef0060c78053bef68c0e1c59e796bf68266aab
Federico Ressi <fressi@redhat.com>
2019-08-12 06:48:14 +0200

    Create unittest for tripleo tobiko.conf section
    Change-Id: Iba17e686a8bcce27c41cc9d36be08d397763305d

---
+++ tobiko/tests/unit/tripleo/test_config.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Red Hat
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import absolute_import
+
+import six
+
+from tobiko import config
+from tobiko.tests import unit
+
+
+CONF = config.CONF
+
+
+class UndercloudConfigTest(unit.TobikoUnitTest):
+
+    conf = CONF.tobiko.tripleo
+
+    def test_undercloud_ssh_hostname(self):
+        value = self.conf.undercloud_ssh_hostname
+        if value is not None:
+            self.assertIsInstance(value, six.string_types)
+
+    def test_undercloud_ssh_port(self):
+        value = self.conf.undercloud_ssh_port
+        self.assertIsInstance(value, int)
+        self.assertIn(value, six.moves.range(1, 2 ** 16))
+
+    def test_undercloud_ssh_username(self):
+        self.assertIsInstance(self.conf.undercloud_ssh_username,
+                              six.string_types)
+
+    def test_ssh_key_filename(self):
+        self.assertIsInstance(self.conf.ssh_key_filename,
+                              six.string_types)
+
+    def test_undercloud_rcfile(self):
+        self.assertIsInstance(self.conf.undercloud_rcfile,
+                              six.string_types)
+
+    def test_overcloud_rcfile(self):
+        self.assertIsInstance(self.conf.overcloud_rcfile,
+                              six.string_types)
