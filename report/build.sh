c44d406360881e9b0ed44a2327e10c20a84eb6a0
Federico Ressi <fressi@redhat.com>
2019-09-30 08:11:21 +0200

    Generate subunit results XML file report
    Change-Id: Ibff0dd636d992a5c14c2fc39d55e0f368dc5e792

--- report/build.sh
+++ report/build.sh
@@ -9,11 +9,14 @@ TESTR_DIR=${TESTR_DIR:-$(pwd)}
 BUILD_DIR=${BUILD_DIR:-${TESTR_DIR}}
 SUBUNIT_FILE=${SUBUNIT_FILE:-${BUILD_DIR}/last.subunit}
 TESTR_RESULTS_HTML=${TESTR_RESULTS_HTML:-${BUILD_DIR}/tobiko_results.html}
+TESTR_RESULTS_XML=${TESTR_RESULTS_XML:-${BUILD_DIR}/tobiko_results.xml}
+
 
 make_testr_results_html() {
     mkdir -p "$(dirname ${SUBUNIT_FILE})"
     (cd "${TESTR_DIR}" && stestr last --subunit) > "${SUBUNIT_FILE}"
     (cd "${BUILD_DIR}" && subunit2html "${SUBUNIT_FILE}" "${TESTR_RESULTS_HTML}")
+    (cd "${BUILD_DIR}" && subunit2html "${SUBUNIT_FILE}" "${TESTR_RESULTS_XML}")
 }
 
 make_testr_results_html
