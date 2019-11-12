bba4c4cb870c381f45f0a233f87c14034380a978
Federico Ressi <fressi@redhat.com>
2019-11-12 17:14:35 +0100

    Update Nova client
    Change-Id: I8ab987c95004703458cc80e169295814a7641a92

--- tobiko/openstack/nova/__init__.py
+++ tobiko/openstack/nova/__init__.py
@@ -19,17 +19,19 @@ from tobiko.openstack.nova import _server
 
 
 CLIENT_CLASSES = _client.CLIENT_CLASSES
+get_console_output = _client.get_console_output
 get_nova_client = _client.get_nova_client
+get_server = _client.get_server
+find_hypervisor = _client.find_hypervisor
+find_server = _client.find_server
+HasNovaClientMixin = _client.HasNovaClientMixin
 list_hypervisors = _client.list_hypervisors
 list_servers = _client.list_servers
-find_server = _client.find_server
 nova_client = _client.nova_client
 NovaClientFixture = _client.NovaClientFixture
-find_hypervisor = _client.find_hypervisor
-get_console_output = _client.get_console_output
-get_server = _client.get_server
 
 skip_if_missing_hypervisors = _hypervisor.skip_if_missing_hypervisors
 
 find_server_ip_address = _server.find_server_ip_address
+HasServerMixin = _server.HasServerMixin
 list_server_ip_addresses = _server.list_server_ip_addresses
