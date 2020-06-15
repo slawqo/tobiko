f530416164c18ad34e576ae7c9a2d2639e2dc7eb
Federico Ressi <fressi@redhat.com>
2020-06-15 16:33:31 +0200

    Add tool to install bindeps
    Change-Id: I1d6152924cffa168ad3199cf97506ac597257e45

---
+++ tools/install-bindeps.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+
+INSTALL_PACKAGE=$(which yum || which apt)
+
+if ! tox -e bindep ; then
+  .tox/bindep/bin/bindep -b | xargs -r "${INSTALL_PACKAGE}" install -y
+  .tox/bindep/bin/bindep
+fi
