9c2a2e749dc16916cbca55f67e4e6ee3c190f6d3
Federico Ressi <fressi@redhat.com>
2020-12-08 15:40:21 +0100

    Pass broken jobs with Python 3.8 (Ubuntu Focal and Fedora 32)
    - Pass linters and lower-constrains jobs with Python 3.8
    - Use master version of pylint only with python version >= 3.9
    - Update requirements files
    - Fix install_bindeps.sh
    - Disable running DevStack jobs on ubuntu focal nodes
    
    Change-Id: Ie745fe5b5a08fe088fcf39edb312170c6cbdc5bb

--- tools/install-bindeps.sh
+++ tools/install-bindeps.sh
@@ -5,6 +5,6 @@ set -ex
 INSTALL_PACKAGE=$(which yum || which apt)
 
 if ! tox -e bindep ; then
-  .tox/bindep/bin/bindep -b | xargs -r "${INSTALL_PACKAGE}" install -y
+  .tox/bindep/bin/bindep -b | xargs -r sudo "${INSTALL_PACKAGE}" install -y
   .tox/bindep/bin/bindep
 fi
