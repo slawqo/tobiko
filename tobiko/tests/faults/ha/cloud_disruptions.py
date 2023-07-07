3a610e1b2d597c6129cc4733aa97d6609b02084f
Eduardo Olivares <eolivare@redhat.com>
2023-07-07 16:03:00 +0200

    Adapt test_soft_reboot_computes_recovery to Amphora LBs
    When an Amphora LB exists this test should reboot the compute nodes
    sequentially. Otherwise the Amphora LBs status is set to ERROR.
    
    Change-Id: I897c608c87a20d912f44800d40f45e4dc81e4d55

--- tobiko/tests/faults/ha/cloud_disruptions.py
+++ tobiko/tests/faults/ha/cloud_disruptions.py
@@ -427,26 +427,36 @@ def network_undisrupt_controllers_non_main_vip():
                                 inverse=True)
 
 
-def reset_all_compute_nodes(hard_reset=False):
+def reset_all_compute_nodes(hard_reset=False, sequentially=False):
 
     # reboot all computes and wait for ssh Up on them
-    # hard reset is simultaneous while soft is sequential
+    def _check_compute(ssh_client):
+        compute_checked = sh.execute(
+            "hostname", ssh_client=ssh_client, expect_exit_status=None).stdout
+        LOG.info('{} is up '.format(compute_checked))
+
     if hard_reset:
         reset_method = sh.hard_reset_method
     else:
         reset_method = sh.soft_reset_method
-    for compute in topology.list_openstack_nodes(group='compute'):
+
+    nodes = topology.list_openstack_nodes(group='compute')
+    for compute in nodes:
         # using ssh_client.connect we use a fire and forget reboot method
         sh.reboot_host(ssh_client=compute.ssh_client, wait=False,
                        method=reset_method)
         LOG.info('reboot exec:  {} on server: {}'.format(reset_method,
                                                          compute.name))
         tobiko.cleanup_fixture(compute.ssh_client)
+        if sequentially:
+            # without the sleep, the command succeeds right after the reboot
+            # - i.e. the node had not been stopped yet
+            time.sleep(5)
+            _check_compute(compute.ssh_client)
 
-    for compute in topology.list_openstack_nodes(group='compute'):
-        compute_checked = sh.execute("hostname", ssh_client=compute.ssh_client,
-                                     expect_exit_status=None).stdout
-        LOG.info('{} is up '.format(compute_checked))
+    if not sequentially:
+        for compute in nodes:
+            _check_compute(compute.ssh_client)
 
 
 def reset_ovndb_pcs_master_resource():
